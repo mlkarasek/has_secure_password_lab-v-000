01168c82aeac7a258af0dfa21ef323749b0d2c6d
Monica  Karasek <mlkarasek@gmail.com>
2018-09-01 23:04:50 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
    get('/login', {to: 'sessions#new'})
    post '/login', to: 'sessions#create'
-
    get '/signup', to: 'users#new'
    post '/signup', to: 'users#create'
 
