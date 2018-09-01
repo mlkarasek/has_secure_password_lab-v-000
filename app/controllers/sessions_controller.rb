53994e76ee1f2cb77e5479f6774b6b730003ca55
Monica  Karasek <mlkarasek@gmail.com>
2018-09-01 23:07:38 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+ 	def new
+     	@user = User.new
+   	end
+
+   	def create
+     	@user = User.find_by(name: params[:user][:name])
+     	if @user
+       		return head(:forbidden) unless @user.authenticate(params[:user][:password])
+       		session[:user_id] = @user.id
+       		redirect_to root_path
+     	else
+       		redirect_to login_path
+     	end
+   	end
+
+   	def destroy
+   	end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'welcome#index'
+   get('/login', {to: 'sessions#new'})
+   post '/login', to: 'sessions#create'
+   get '/signup', to: 'users#new'
+   post '/signup', to: 'users#create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
